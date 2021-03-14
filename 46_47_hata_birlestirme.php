ddb4e4f7af5af156495c8f780f9bb24972179895
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 21:18:04 +0300

    Create 46_47_hata_birlestirme.php

---
+++ 46_47_hata_birlestirme.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<?php
+
+
+// echo "beyza subasi yazdıktan sonra " unuttuysam hata verir ve hatanın da hangi satırda olduğunu söyler
+
+// echo "beyza subasi" yazdıktan sonra ; unuttuysam bu satırda hata vermez ama bir sonraki satırımda hata çıkar
+
+
+//birleştirme operatörü
+echo "beyza"; echo "subasi"; 
+//yerine
+echo "beyza"."subasi";
+
+
+
+
+
+?>
+
+
+</body>
+</html>
