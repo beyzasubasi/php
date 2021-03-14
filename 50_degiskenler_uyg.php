3c2c50cefee0b000ced76f4b7b6b969844dd7c65
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 21:18:08 +0300

    Create 50_degiskenler_uyg.php

---
+++ 50_degiskenler_uyg.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<?php 
+
+$ad="Beyza";
+$soyad="Subaşı";
+$no=5075051206;
+
+echo "<h1>Bİlgilerim</h1>";
+echo "<hr>";
+echo"Adınız ve Soyadınız: ".$ad." ".$soyad."<br>";
+echo "Numaranız: ".$no;
+
+
+?>
+
+
+
+</body>
+</html>
