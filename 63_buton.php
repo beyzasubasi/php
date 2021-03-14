f3fe28f752401773e740c81f1198338300de9c73
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 21:18:30 +0300

    Create 63_buton.php

---
+++ 63_buton.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<?php
+
+echo $_GET['ad'];     
+echo "<br>";
+echo $_GET['soyad'];
+$kullanici_id=151;
+
+?>
+
+
+<form action="63_tutulan_deger.php" method="GET">
+
+	Ad <input type="text" name="ad" placeholder="Adınızı Giriniz..">
+	SoyAd <input type="text" name="soyad" placeholder="Soy Adınızı Giriniz..">
+	<input type="submit" value="Formu Gönder.." name="">
+	
+</form>
+
+<a href="63_tutulan_deger.php?kullanici_id=<?php echo $kullanici_id ?>"><button>Kullanıcıyı Sil</button></a>
+
+</body>
+</html>
