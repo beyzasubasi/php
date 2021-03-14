06e351743f8613940c252b29b2fd11ce93911040
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 21:18:43 +0300

    Create 68_switch_case.php

---
+++ 68_switch_case.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<?php
+
+//SWITCH CASE
+
+	$sayi=5;
+
+	switch($sayi)
+	{
+		case '1':
+			echo "1";
+			break;
+
+			case '2';
+			echo "2";
+			break;
+
+			case '3';
+			echo "3";
+			break;
+
+			case '4';
+			echo "4";
+			break;
+
+			case '5';
+			echo "5";
+			break;
+
+		default:
+			echo "Başka Bir Şey..";
+			break;
+
+	}
+
+?>
+
+
+</body>
+</html>
