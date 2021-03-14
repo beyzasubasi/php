1b1dca87af8dbcabc516702ace09aaa2e1747400
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 21:18:36 +0300

    Create 65_if_elif_else.php

---
+++ 65_if_elif_else.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+
+<?php
+
+//IF KOŞULU 
+$sayi=5;
+if ($sayi==5) 
+{
+	echo "Bu sayı 5tir.";
+}
+
+//  ==   eşit ise
+//  ===  aynısı ise
+
+ 
+
+echo "<br>";
+//ELSEIF KOŞULU
+$sayi=5;
+if ($sayi==5) 
+{
+	echo "Bu sayı 5tir.";
+}
+elseif($sayi<=5)
+{
+	echo "Bu sayı 5ten küçük veya eşittir.";
+}
+
+
+
+//ELSE
+$sayi=400;
+
+if ($sayi==5) 
+{
+	echo "Bu sayı 5tir.";
+}
+elseif($sayi<5)
+{
+	echo "Bu sayı 5ten küçük veya eşittir.";
+}
+elseif($sayi>5)
+}
+	echo "Bu sayı 5ten büyüktür.";
+}
+else
+{
+	echo "Başka bir şey..";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
