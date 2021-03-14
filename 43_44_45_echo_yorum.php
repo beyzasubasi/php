589dac1722d93146eaff9ad40acf656224d16dea
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 21:18:01 +0300

    Create 43_44_45_echo_yorum.php

---
+++ 43_44_45_echo_yorum.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<!-- PHP KODU -->
+<?php ?>
+
+
+<!-- ekrana bir şey basmak istiyorsak echo komutu -->
+<?php echo "beyza subaşı"; ?>
+<!-- print "beyza subaşı" şeklinde de olur ama echoyu bilelim bunu daha sonra detaylandıracağız -->
+
+
+
+<!-- istisnalar dışında bi kod morardıysa sorun vardır -->
+
+<!-- php nin içinde html yorum satırı kullanılmaz hata verir 
+<?php  // <!-- bu bir yorum satırıdır --> ?>
+ 
+
+
+php nin içinde yorum satırı olarak:
+// bu bir yorum satırıdır
+# bu bir yorum satırıdır
+/* burada birden fazla satır yorum satırı olur */
+
+-->
+
+</body>
+</html>
+
+
+
+
